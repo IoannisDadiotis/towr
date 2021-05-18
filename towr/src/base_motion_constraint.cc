a30ee37a07d0348c767d83736b8289b25f912386
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-05-18 11:39:27 +0200

    initial base height can be a bit lower

--- towr/src/base_motion_constraint.cc
+++ towr/src/base_motion_constraint.cc
@@ -48,10 +48,10 @@ BaseMotionConstraint::BaseMotionConstraint (double T, double dt,
   node_bounds_.at(AY) = Bounds(-dev_rad, dev_rad);
   node_bounds_.at(AZ) = ifopt::NoBound;//Bounds(-dev_rad, dev_rad);
 
-  double z_init = base_linear_->GetPoint(0.0).p().z();
+  double z_init = base_linear_->GetPoint(0.0).p().z(); // deviations
   node_bounds_.at(LX) = ifopt::NoBound;
   node_bounds_.at(LY) = ifopt::NoBound;//Bounds(-0.05, 0.05);
-  node_bounds_.at(LZ) = Bounds(z_init-0.02, z_init+0.1); // allow to move dev_z cm up and down
+  node_bounds_.at(LZ) = Bounds(z_init-0.1, z_init+0.02); // allow to move dev_z cm up and down
 
   int n_constraints_per_node = node_bounds_.size();
   SetRows(GetNumberOfNodes()*n_constraints_per_node);
